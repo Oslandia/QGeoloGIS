Fix X values computation in data_interface

qgeologis/data_interface.py
@@ -228,7 +228,7 @@ class FeatureData(DataInterface):
                                for value in raw_data[1:-1].split(",")]
 
         if self.__x_values is None:
-            self.__x_values = np.linspace(self.__x_start, self.__x_delta * len(self.__y_values), len(self.__y_values))
+            self.__x_values = np.linspace(self.__x_start, self.__x_start + self.__x_delta * len(self.__y_values), len(self.__y_values))
 
         self.__x_min, self.__x_max = (min(self.__x_values), max(self.__x_values))
         self.__y_min, self.__y_max = (min(self.__y_values), max(self.__y_values))